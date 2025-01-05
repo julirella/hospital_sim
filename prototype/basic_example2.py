da3a82afc0a5373f2a474473da867cad0f3440eb
Julia Grandcourtova <grandjul@fit.cvut.cz>
2025-01-05 18:25:24 +0100

    move prototype into its own dir

---
+++ prototype/basic_example2.py
@@ -0,0 +1,280 @@
+from enum import IntEnum
+import math
+import heapq
+from sortedcontainers import SortedDict
+import numpy as np
+import pandas as pd
+
+TIME_WITH_PATIENT = 10
+REACTION_TIME = 1
+
+class NurseMovement(IntEnum):
+    REACTING = 1
+    TO_PATIENT = 2
+    WITH_PATIENT = 3
+    FROM_PATIENT = 4
+
+class Event:
+    def __init__(self, time):
+        self.time = time
+        self.log = {}
+    
+    def handle_event(self, nurses, free_nurses, busy_nurses):
+        raise NotImplementedError()
+
+class Request(Event):
+    def __init__(self, time, patient, req_id):
+        super().__init__(time)
+        self.patient = patient
+        self.id = req_id
+
+    def handle_event(self, nurses, free_nurses, busy_nurses):
+        print(self.time, "request from patient {}".format(self.patient.id))
+        nurse_id = self.patient.nurse_id
+        assigned_nurse = nurses[nurse_id]
+        
+        if len(free_nurses) == 0:
+            min_busy_end = math.inf
+            for busy_nurse_id in busy_nurses: #could just iterate over nurses at this point
+                busy_nurse = nurses[busy_nurse_id] 
+                if busy_nurse.busy_end_time < min_busy_end:
+                    min_busy_end = busy_nurse.busy_end_time
+            print(self.time, "all nurses busy, request pushed back to {}".format(min_busy_end))
+            self.log = {'time' : self.time, 'type' : "request", "request_id" : self.id, 'patient_id' : self.patient.id, 'patient_dst' : self.patient.office_dst, 'requested_nurse_id' : self.patient.nurse_id, 'pushed_back' : True, 'pushed_back_time' : min_busy_end}
+            return Request(min_busy_end, self.patient, self.id)
+        else:
+            if not assigned_nurse.is_free:
+                nurse_id = free_nurses.pop() 
+                assigned_nurse = nurses[nurse_id]
+                print(self.time, "nurse {} chosen for patient {} because nurse {} is busy".format(nurse_id, self.patient.id, self.patient.nurse_id))
+                self.log = {'time' : self.time, 'type' : "request", "request_id" : self.id, 'patient_id' : self.patient.id, 'patient_dst' : self.patient.office_dst, 'requested_nurse_id' : self.patient.nurse_id, 'chosen_nurse_id' : nurse_id, 'pushed_back' : False}
+            else:
+                free_nurses.remove(nurse_id)
+                print(self.time, "nurse {} chosen for patient {}".format(nurse_id, self.patient.id))
+                self.log = {'time' : self.time, 'type' : "request", "request_id" : self.id, 'patient_id' : self.patient.id, 'patient_dst' : self.patient.office_dst, 'requested_nurse_id' : nurse_id, 'chosen_nurse_id' : nurse_id, 'pushed_back' : False}
+            nextPhaseStart = assigned_nurse.start_request(self.patient, self.time, self)
+            busy_nurses.add(nurse_id)
+            return NursePhase(nextPhaseStart, assigned_nurse)
+
+class NursePhase(Event):
+    def __init__(self, time, nurse):
+        super().__init__(time)
+        self.nurse = nurse
+    def handle_event(self, nurses, free_nurses, busy_nurses):
+        self.log = self.nurse.next_phase(self.time)
+        if self.nurse.is_free:
+            nurse_id = self.nurse.id
+            busy_nurses.remove(nurse_id)
+            free_nurses.add(nurse_id)
+            return None
+        else:
+            return NursePhase(self.nurse.nextPhaseStart, self.nurse)
+
+
+class Patient:
+    def __init__(self, id, nurse_id, office_dst):
+        self.id = id
+        self.nurse_id = nurse_id
+        self.office_dst = office_dst
+        
+
+class Nurse:
+    def __init__(self, id):
+        self.id = id
+        self.is_free = True
+    
+    def start_request(self, patient, time, request):
+        self.assigned_req = request
+        self.is_free = False
+        self.move_phase = NurseMovement.REACTING
+        self.patient_id = patient.id
+        self.nextPhaseStart = time + REACTION_TIME
+        self.patient_dst = patient.office_dst
+        self.busy_end_time = time + REACTION_TIME + self.patient_dst * 2 + TIME_WITH_PATIENT
+        return self.nextPhaseStart
+    
+    def next_phase(self, time):
+        if self.move_phase == NurseMovement.FROM_PATIENT:
+            self.is_free = True
+            print(time, "nurse {} returned from patient {}".format(self.id, self.patient_id))
+            return {'time' : time, 'type' : "nurse_phase", "request_id" : self.assigned_req.id, 'nurse_id' : self.id, 'patient_id' : self.patient_id, 'patient_dst' : self.patient_dst, 'nurse_phase' : "returned"}
+        else:
+            self.move_phase += 1
+            if self.move_phase == NurseMovement.TO_PATIENT:
+                self.nextPhaseStart = time + self.patient_dst
+                print(time, "nurse {} departing to patient {}".format(self.id, self.patient_id))
+                return {'time' : time, 'type' : "nurse_phase", "request_id" : self.assigned_req.id, 'nurse_id' : self.id, 'patient_id' : self.patient_id, 'patient_dst' : self.patient_dst, 'nurse_phase' : "departing"}
+            elif self.move_phase == NurseMovement.FROM_PATIENT:
+                self.nextPhaseStart = time + self.patient_dst
+                print(time, "nurse {} dealt with request from patient {}".format(self.id, self.patient_id))
+                return {'time' : time, 'type' : "nurse_phase", "request_id" : self.assigned_req.id, 'nurse_id' : self.id, 'patient_id' : self.patient_id, 'patient_dst' : self.patient_dst, 'nurse_phase' : "dealt with request"}
+            else:
+                self.nextPhaseStart = time + TIME_WITH_PATIENT
+                print(time, "nurse {} arrived at patient {}".format(self.id, self.patient_id))
+                return {'time' : time, 'type' : "nurse_phase", "request_id" : self.assigned_req.id, 'nurse_id' : self.id, 'patient_id' : self.patient_id, 'patient_dst' : self.patient_dst, 'nurse_phase' : "arrived"}
+        
+class EventQueue:
+    def __init__(self, request_tuples, patients):
+        #for now, constructs Request objects from tuples
+        #later maybe accept list of requests instead
+        self.events = SortedDict()
+        for i, req_tuple in enumerate(request_tuples):
+            time = req_tuple[0] 
+            patient_id = req_tuple[1]
+            req = Request(time, patients[patient_id], i)
+            self.insert(req)
+
+    def first_time(self):
+        return next(iter(self.events.items())) #returns first (time, events)
+    def first(self):
+        time, events = self.first_time()
+        return time, events[0]
+
+    def insert(self, event):
+        #insert a constructed Event object if event is not None
+        if event is not None:
+            time = event.time
+            if time in self.events:
+                self.events[time].append(event)
+            else:
+                self.events[time] = [event]
+    def empty(self):
+        return len(self.events) == 0
+
+    def popFirst(self):
+        time, events = self.first_time()
+        if len(events) == 1:
+            event = self.events.pop(time)[0]
+        else:
+            event = events.pop(0)
+        return time, event
+
+
+class Simulator:
+    def __init__(self, requests, nurses, patients):
+        self.events = EventQueue(requests, patients)
+        # print(self.events.items())
+        self.patients = patients
+        self.nurses = nurses #.copy() ?
+        self.busy_nurses = set()
+        self.free_nurses = set()
+        for nurse in nurses:
+            self.free_nurses.add(nurse.id)
+
+        self.log = []
+    def simulate(self) -> list[dict]:
+        while not self.events.empty():
+            _, event = self.events.popFirst()
+            newEvent = event.handle_event(self.nurses, self.free_nurses, self.busy_nurses)
+            self.events.insert(newEvent)
+            self.log.append(event.log)
+        
+        return self.log
+        # df.to_csv('out.csv')
+
+def generate_requests(patientAmount: int) -> list[float, int]:
+    requests = []
+    for patient_id in range(patientAmount):
+        request_amount = np.random.randint(30, 70)
+        for _ in range(request_amount):
+            req_time = np.random.uniform(0, 3600)
+            requests.append((req_time, patient_id))
+    
+    requests.sort()
+    print(requests)
+    print(len(requests))
+    return requests
+
+def create_nurses_and_patients(nurseAmount: int, patientAmount: int, randomNurses: bool=False) -> tuple[list[Nurse], list[Patient]]:
+    nurses = []
+    for i in range(nurseAmount):
+        nurses.append(Nurse(i))
+
+    patients = []
+    for i in range(patientAmount):
+        office_dst = (i+1)*5 #first patient is 5s away, each following is 5s further than the previous
+        if(randomNurses):
+            nurse_id = np.random.randint(0, nurseAmount)
+        else:
+            nurse_id = i % len(nurses)        
+        patients.append(Patient(i, nurse_id, office_dst))
+    
+    return nurses, patients
+
+def main():
+    # print("Test 1") #one patient, one nurse
+    # nurses = []
+    # for i in range(1):
+    #     nurses.append(Nurse(i))
+    # patients = []
+    # for i in range(1):
+    #     patients.append(Patient(i, i % len(nurses)))
+    # requests = [(1, 0)]
+    # # requests = [(1, 0), (2, 1), (1, 1)]
+
+    # generate_requests(2)
+    # sim = Simulator(requests, nurses, patients)
+    # sim.simulate()
+
+    # print("Test 2")
+    # requests = [(1, 0), (50, 0)] #time, patient id
+    # sim = Simulator(requests, nurses, patients)
+    # sim.simulate()
+
+    # print("Test 3")
+    # requests = [(1, 0), (42, 0)] #time, patient id
+    # sim = Simulator(requests, nurses, patients)
+    # sim.simulate()
+
+    # print("Test 4") #multiple patients
+    # nurses = []
+    # for i in range(1):
+    #     nurses.append(Nurse(i))
+    # patients = []
+    # for i in range(3):
+    #     patients.append(Patient(i, i % len(nurses)))
+
+    # requests = [(1, 0), (50, 1)] #time, patient id
+    # sim = Simulator(requests, nurses, patients)
+    # sim.simulate()
+
+    # print("Test 5")
+    # requests = [(1, 0), (42, 1)] #time, patient id
+    # sim = Simulator(requests, nurses, patients)
+    # sim.simulate()
+
+    # print("Test 6") #multiple nurses
+    nurses = []
+    for i in range(2):
+        nurses.append(Nurse(i))
+    patients = []
+    for i in range(3):
+        patients.append(Patient(i, i % len(nurses)))
+
+    # requests = [(1, 0), (50, 1)] #time, patient id
+    # sim = Simulator(requests, nurses, patients)
+    # sim.simulate()
+
+    # print("Test 7") #overlapping request times
+    # requests = [(1, 0), (25, 1)] #time, patient id
+    # sim = Simulator(requests, nurses, patients)
+    # sim.simulate()
+
+    # print("Test 8") #nurse reassignment
+    # requests = [(1, 0), (25, 2)] #time, patient id
+    # sim = Simulator(requests, nurses, patients)
+    # sim.simulate()
+
+    print("Test 9") #no nurses free when a request comes
+    requests = [(1, 0), (25, 2), (26, 1)] #time, patient id
+    sim = Simulator(requests, nurses, patients)
+    sim.simulate()
+
+    # print("Test 10") #no nurses free when a request comes
+    # requests = generate_requests(len(patients))
+    # sim = Simulator(requests, nurses, patients)
+    # sim.simulate()
+
+
+if __name__ == "__main__":
+    main()
---
+++ prototype/plot.py
@@ -0,0 +1,248 @@
+import pygame
+from pygame.locals import *
+import sys
+import basic_example2 as be
+import pandas as pd
+import math
+from time import sleep
+from sortedcontainers import SortedList
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+PATIENT_OFFSET = 100
+IMG_TOP = 50
+IMG_LEFT = 50
+OFFICE_WIDTH = 100
+DST_OFFSET = 20
+
+#colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+class Event:
+    def __init__(self, event_from_table: dict):
+        self.time  = event_from_table['time']
+        self.type = event_from_table['type']
+        self.request_id = event_from_table['request_id']
+        self.patient_id = event_from_table['patient_id']
+        self.patient_dst = event_from_table['patient_dst']
+        self.requested_nurse_id	 = self.to_int(event_from_table['requested_nurse_id'])
+        self.chosen_nurse_id = self.to_int(event_from_table['chosen_nurse_id'])
+        self.nurse_id = self.to_int(event_from_table['nurse_id'])
+        self.pushed_back = event_from_table['pushed_back']
+        self.nurse_phase = event_from_table['nurse_phase']
+
+        self.first_occurence = True
+
+    def to_int(self, num):
+        #TODO some less hacky solution
+        if math.isnan(num):
+            return -1
+        else:
+            return int(num)
+
+class NursePos:
+    def __init__(self, colour: pygame.Color):
+        self.pos = 0
+        self.patient = None
+        self.dir = 0 #1 forward, -1 back, 0 nothing
+        self.at_patient = False
+        self.phase_time_left = 0 #maybe useless
+        self.colour = colour
+    def move(self):
+        if self.patient != None and self.at_patient == False:
+            self.pos += self.dir
+            self.phase_time_left -= 1
+
+class PatientInfo:
+    def __init__(self, colour: pygame.Color, office_dst: int):
+        self.req_cnt = 0
+        self.colour = colour
+        self.office_dst = office_dst
+
+class State:
+    def __init__(self, nurse_positions: list[NursePos], patient_infos: list[PatientInfo]):
+        self.nurse_positions = nurse_positions
+        self.patient_infos = patient_infos
+        self.requests_waiting = SortedList()
+    
+    def tick_clock(self):
+        for nurse_pos in self.nurse_positions:
+                nurse_pos.move()
+    
+    def handle_event(self, event: Event):
+        if event.type == 'request': #patient assigned but not moving 
+            waiting = self.requests_waiting.__contains__(event.request_id)
+            if not waiting:
+                self.patient_infos[event.patient_id].req_cnt += 1
+                self.requests_waiting.add(event.request_id)
+            if event.pushed_back == False:
+                nurse_id = event.chosen_nurse_id
+                nurse_pos = self.nurse_positions[nurse_id]
+                nurse_pos.patient = event.patient_id
+                if waiting:
+                    self.requests_waiting.remove(event.request_id)
+
+        elif event.type == 'nurse_phase':
+            nurse_id = event.nurse_id
+            nurse_pos = self.nurse_positions[nurse_id]
+
+            if event.nurse_phase == 'departing':
+                nurse_pos.dir = 1
+                nurse_pos.phase_time_left = event.patient_dst
+            elif event.nurse_phase == 'arrived':
+                nurse_pos.at_patient = True
+                nurse_pos.dir = 0
+                nurse_pos.phase_time_left = 20 #TODO: have this as some global variable
+                self.patient_infos[event.patient_id].req_cnt -= 1
+            elif event.nurse_phase == 'dealt with request':
+                nurse_pos.at_patient = False
+                nurse_pos.dir = -1
+                nurse_pos.phase_time_left = event.patient_dst
+            elif event.nurse_phase == 'returned':
+                nurse_pos.dir = 0
+                nurse_pos.patient = None
+
+    
+    def display(self, surf: pygame.Surface):
+        #nurse dots
+        free_nurses = 0
+        for nurse_pos in self.nurse_positions:
+            patient = nurse_pos.patient
+            if patient != None:
+                pos = nurse_pos.pos
+                pygame.draw.circle(surf, nurse_pos.colour, (OFFICE_WIDTH + DST_OFFSET*pos, patient * PATIENT_OFFSET + PATIENT_OFFSET // 2), 5)
+            else:
+                pygame.draw.circle(surf, nurse_pos.colour, (30, 30 + free_nurses * 30), 10)
+                free_nurses += 1
+
+        #TODO: display something to show patient waiting for request fulfilment
+        wait_font = pygame.font.Font('freesansbold.ttf', 20)
+        for i, patient_info in enumerate(self.patient_infos):
+            text = wait_font.render(str(patient_info.req_cnt), True, WHITE)
+            surf.blit(text, (OFFICE_WIDTH + DST_OFFSET*patient_info.office_dst + 20, i * PATIENT_OFFSET + PATIENT_OFFSET // 2 - 10))
+
+class Plot:
+    def __init__(self, events: list[dict], nurse_amount: int, patient_amount: int, patient_dsts: list[int], patient_nurses: list[int]):
+        self.events = []
+        for event in events:
+            self.events.append(Event(event))
+
+        #create states or accept them as arguments
+        self.nurse_amount = nurse_amount
+        self.patient_amount = patient_amount
+        self.patient_dsts = patient_dsts
+        
+        nurse_positions = []
+        for i in range(self.nurse_amount):
+            colour = self.int_to_colour(i, nurse_amount)
+            nurse_positions.append(NursePos(colour))
+        patient_infos = []
+        for i in range(self.patient_amount):
+            colour = self.int_to_colour(patient_nurses[i], nurse_amount)
+            patient_infos.append(PatientInfo(colour, patient_dsts[i]))
+        self.state = State(nurse_positions, patient_infos)
+
+        self.rect_height = max(self.nurse_amount, self.patient_amount) * PATIENT_OFFSET
+        self.nurse_rect = pygame.Rect(0, 0, OFFICE_WIDTH, self.rect_height)
+
+    def int_to_colour(self, num: int, total_colours: int) -> pygame.Color:
+        step = 360 // total_colours
+        colour = pygame.Color(0, 0, 0, 0)
+        colour.hsva = (step * num, 100, 100, 50)
+        return colour
+
+    def display_init(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.info_surf = pygame.Surface((200, 50))
+        self.image_surf = pygame.Surface((SCREEN_WIDTH - 100, SCREEN_HEIGHT))
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
+        self.clock = pygame.time.Clock()
+        self.display_static()
+        pygame.display.flip()
+
+    def display_static(self):
+        self.image_surf.fill(BLACK)
+        pygame.draw.rect(surface=self.image_surf, color=WHITE, rect=self.nurse_rect)
+        for i, patient_info in enumerate(self.state.patient_infos):
+            pygame.draw.circle(self.image_surf, patient_info.colour, (OFFICE_WIDTH + DST_OFFSET*patient_info.office_dst, i * PATIENT_OFFSET + PATIENT_OFFSET // 2), 15)
+
+            for j in range(1, patient_info.office_dst + 1):
+                pygame.draw.circle(self.image_surf, WHITE, (OFFICE_WIDTH + DST_OFFSET*j, i * PATIENT_OFFSET + PATIENT_OFFSET // 2), 5)
+        
+        self.screen.blit(self.image_surf, (50, 50))
+        # pygame.display.flip() #fixes dots staying on office edge. Probably eventually put static stuff on a different surface and just blit it
+        
+
+    def display_info(self, time: int):
+        self.info_surf.fill(WHITE)
+        text = self.font.render("time: " + str(time), True, BLACK)
+        self.info_surf.blit(text, (5, 5))  # Position: (50, 50)
+        self.screen.blit(self.info_surf, (SCREEN_WIDTH - 200, 0))
+
+    def run(self):
+        time = 0
+        event_num = 0
+        self.display_init()
+        while True:
+            for event in pygame.event.get():              
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.state.tick_clock() #should this be after handle events? Currently only works before
+
+            #handle all events in last second
+            while event_num < len(self.events) and self.events[event_num].time <= time:
+                self.state.handle_event(self.events[event_num])
+                event_num += 1 #will probably go out of range after last event
+            self.display_static() #maybe only overwrite the parts that need to be
+            self.display_info(time)
+
+            self.state.display(self.image_surf)
+            self.screen.blit(self.image_surf, (50, 50)) #this is being blitted in 2 different places!!
+            pygame.display.flip()
+            time += 1
+            self.clock.tick(2)
+
+
+
+def main():
+    #TODO sort out how to run this from jupyter
+    nurse_amount = 3
+    patient_amount = 3
+    nurses, patients = be.create_nurses_and_patients(nurseAmount=nurse_amount, patientAmount=patient_amount)
+    # requests = [(1, 0), (25, 2), (26, 1), (50, 0)] #time, patient id
+    requests = [(1, 0), (2, 1), (3, 1), (4, 0)] #time, patient id
+    patient_dsts = []
+    patient_nurses = []
+    for patient in patients:
+        patient_dsts.append(patient.office_dst)
+        patient_nurses.append(patient.nurse_id)
+    sim = be.Simulator(requests, nurses, patients)
+    out = sim.simulate()
+    df2 = pd.DataFrame(out) #TODO design some structure to not have to go through df to keep NaNs
+    outDict = df2.to_dict(orient='records') 
+    plot = Plot(outDict, nurse_amount, patient_amount, patient_dsts, patient_nurses)
+    plot.run()
+
+    # nurse_amount = 3
+    # patient_amount = 5
+    # nurses, patients = be.create_nurses_and_patients(nurseAmount=nurse_amount, patientAmount=patient_amount)
+    # requests = be.generate_requests(len(patients))
+    # patient_dsts = []
+    # patient_nurses = []
+    # for patient in patients:
+    #     patient_dsts.append(patient.office_dst)
+    #     patient_nurses.append(patient.nurse_id)
+    # sim = be.Simulator(requests, nurses, patients)
+    # df = pd.DataFrame(sim.simulate())
+    # outDict = df.to_dict(orient='records') 
+    # plot = Plot(outDict, nurse_amount, patient_amount, patient_dsts, patient_nurses)
+    # plot.run()
+
+if __name__ == "__main__":
+    main()
