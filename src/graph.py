f98f0cffc93f470edcd704f4142998989ace5bc4
Julia Grandcourtova <grandjul@fit.cvut.cz>
2025-01-13 10:37:49 +0100

    path finding

--- src/graph.py
+++ src/graph.py
@@ -7,13 +7,45 @@ class Graph:
         self.edges = edges
         pass
     
+    def __retrace_path(self, start_id: int, end_id: int, prev: list[int]) -> list[Node]:
+        path = []
+        current_id = end_id
+        while current_id != start_id:
+            path.append(self.nodes[current_id]) 
+            current_id = prev[current_id]
+        path.append(self.nodes[start_id])
+        path.reverse()
+        return path
+    
     def find_path(self, start: Node, end: Node) -> list[Node]:
+        #dijkstra, source: https://courses.fit.cvut.cz/BI-AG1/lectures/media/bi-ag1-p12-handout.pdf
         start_id = start.id
         end_id = end.id
         node_cnt = len(self.nodes)
         
-        path = []
-        prev = [None] * node_cnt
-        dist = SortedDict()
+        open_nodes = set()
+        prev: list[int] = [-1] * node_cnt #predecessors
+        dist = SortedDict() #distances to all nodes
+        open_dist = SortedDict() #distances to open nodes
+
         for i in range(node_cnt):
             dist[i] = float('inf')
+
+        dist[start_id] = 0
+        open_dist[start_id] = 0
+        open_nodes.add(start_id)
+
+        while len(open_nodes) > 0:
+            current_id = open_dist.popitem(0)[0]
+            for neighbour, weight in self.edges[current_id]:
+                neighbour_id = neighbour.id
+                new_dist = dist[current_id] + weight
+                if dist[neighbour_id] > new_dist: #what if neighbour is predecessor of current
+                    dist[neighbour_id] = new_dist
+                    prev[neighbour_id] = current_id
+                    open_dist[neighbour_id] = new_dist
+                    if neighbour_id not in open_nodes:
+                        open_nodes.add(neighbour_id)
+            open_nodes.remove(current_id) 
+        
+        return self.__retrace_path(start_id, end_id, prev)
