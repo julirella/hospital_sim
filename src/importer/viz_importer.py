8bad0346a50d316881db209fc4d4689db3456a8d
Julia Grandcourtova <grandjul@fit.cvut.cz>
2025-02-11 10:54:31 +0100

    import eventLog into visualisation

--- src/importer/viz_importer.py
+++ src/importer/viz_importer.py
@@ -11,11 +11,13 @@ from src.constants import *
 
 class VizImporter(Importer):
 
-    def __init__(self, graph_filename: str, entity_file_name: str, nurse_log_filename: str) -> None:
+    def __init__(self, graph_filename: str, entity_file_name: str, nurse_log_filename: str, event_log_filename: str) -> None:
         super().__init__(graph_filename, entity_file_name)
         self.nurse_log_filename = nurse_log_filename
+        self.event_log_filename = event_log_filename
         self.corridors = []
         self.patients_in_rooms = []
+        self.req_times = {} #dict because not all patients have requests
 
     def import_graphit_graph(self) -> None:
         self.parse_graphit_graph()
@@ -46,7 +48,13 @@ class VizImporter(Importer):
         for i, patient_info in enumerate(patient_lst):
             nurse = patient_info["nurse_id"]
             room = patient_info["room"]
-            patient = VisPatient(self.int_to_colour(nurse, nurse_cnt), room)
+
+            if i in self.req_times:
+                start_times, end_times = self.req_times[i]
+            else:
+                start_times, end_times = [], []
+
+            patient = VisPatient(self.int_to_colour(nurse, nurse_cnt), room, start_times, end_times)
             self.patients_in_rooms[room].append(patient)
             patients.append(patient)
 
@@ -64,6 +72,16 @@ class VizImporter(Importer):
 
         return nurse_dfs
 
+    def import_event_log(self):
+        event_df = pd.read_csv(self.event_log_filename)
+        req_df = event_df[event_df['type'] == "request"]
+        patient_ids = req_df['patient'].unique().tolist()
+        for patient_id in patient_ids:
+            patient_reqs = req_df[req_df['patient'] == patient_ids[patient_id]]
+            start_times = sorted(patient_reqs[patient_reqs['action'] == "planned start"]["time"].tolist())
+            end_times = sorted(patient_reqs[patient_reqs['action'] == "end"]["time"].tolist())
+            self.req_times[patient_id] = start_times, end_times
+
     def import_data(self) -> Visualiser:
         self.import_graphit_graph()
         nurses, patients = self.import_entities()
@@ -78,6 +96,7 @@ class VizImporter(Importer):
         for i, room in enumerate(self.patient_rooms):
             vis_rooms.append(VisRoom(room, pixels_per_meter, self.patients_in_rooms[i]))
 
+        self.import_nurse_log()
         nurse_office = VisRoom(self.nurse_office, pixels_per_meter, [])
 
         dept_map = Map(vis_rooms, nurse_office, self.corridors, nurses, patients, map_width, map_height, pixels_per_meter)
--- src/visualisation/vis_patient.py
+++ src/visualisation/vis_patient.py
@@ -1,4 +1,6 @@
 class VisPatient:
-    def __init__(self, colour, room_number):
+    def __init__(self, colour, room_number, req_start_times, req_end_times):
         self.colour = colour
         self.room_number = room_number
+        self.req_start_times: list[float] = req_start_times
+        self.req_end_times: list[float] = req_end_times
