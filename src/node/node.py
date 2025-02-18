2eb7cfd7625d52c7a718185467ed032b6221b3dd
Julia Grandcourtova <grandjul@fit.cvut.cz>
2025-02-18 09:27:35 +0100

    node properties

--- src/node/node.py
+++ src/node/node.py
@@ -3,8 +3,16 @@ from typing import Self
 class Node:
     def __init__(self, node_id: int, x: float, y: float) -> None:
         self.node_id = node_id
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self) -> float:
+        return self._x
+
+    @property
+    def y(self) -> float:
+        return self._y
 
     def neighbours(self) -> tuple[Self, Self] | None:
         return None
--- src/node/patient_room.py
+++ src/node/patient_room.py
@@ -3,4 +3,8 @@ from .room import Room
 class PatientRoom(Room):
     def __init__(self, node_id: int, x: float, y: float, room_number: int) -> None:
         super().__init__(node_id, x, y)
-        self.room_number = room_number
+        self._room_number = room_number
+
+    @property
+    def room_number(self) -> int:
+        return self._room_number
