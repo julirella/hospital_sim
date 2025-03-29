1810bfc28380cfabdce747b525ced78698e805f5
Julia Grandcourtova <grandjul@fit.cvut.cz>
2025-03-29 18:21:03 +0100

    main to run everything from one place

--- src/main.py
+++ src/main.py
@@ -1,12 +1,40 @@
-# from event.request import Request
+from src.exporter.log_exporter import LogExporter
+from src.importer import SimImporter
+from src.importer.viz_importer import VizImporter
+
+
+class App:
+    def __init__(self, graph_path, people_path, event_path, nurse_output_path, event_output_path):
+        self.graph_path = graph_path
+        self.people_path = people_path
+        self.event_path = event_path
+        self.nurse_output_path = nurse_output_path
+        self.event_output_path = event_output_path
+
+    def run_simulation(self, visualise=False):
+        importer = SimImporter(self.graph_path, self.people_path, self.event_path)
+        sim = importer.import_data()
+        sim.simulate()
+        log_exporter = LogExporter(sim, self.nurse_output_path, self.event_output_path)
+        log_exporter.export_data()
+
+        if visualise:
+            self.run_visualisation()
+
+    def run_visualisation(self):
+        importer = VizImporter(self.graph_path, self.people_path, self.nurse_output_path, self.event_output_path)
+        visualiser = importer.import_data()
+        visualiser.run()
+
 
 def main():
-    print(req)
+    # print(req)
     #load input from file
     #create graph
     #create nurses with plan queues
     #simulate
     #output log to file
+    ...
 
 
 if __name__ == "__main__":
