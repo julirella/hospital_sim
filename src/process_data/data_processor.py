06958e39b0bfc02be9699c70205b09d406d7edf5
Julia Grandcourtova <grandjul@fit.cvut.cz>
2025-03-29 11:12:39 +0100

    data processor for nurse statistics

---
+++ src/process_data/data_processor.py
@@ -0,0 +1,85 @@
+import pandas as pd
+from math import sqrt
+from itertools import chain
+
+from src.importer.gen_importer import GenImporter
+
+class DataProcessor:
+    def __init__(self, nurse_log_path, event_log_path, people_path) -> None:
+        self.nurse_df = pd.read_csv(nurse_log_path)
+        self.event_df = pd.read_csv(event_log_path)
+        gen_importer = GenImporter("", people_path) #it would be better to create a separate importer for people only
+        gen_importer.import_people()
+        self.nurse_patients = gen_importer.nurse_patients
+        self.patient_cnt = gen_importer.patient_cnt
+        
+
+    def row_diff(self, row_num: int, df: pd.DataFrame, col_name: str):
+        #calculates difference between items in col_name in df on rows row_num and the one before it
+        #the column must be of a numerical type
+        if row_num < 1:
+            raise Exception("Row_num must be at least 1 (second row)")
+        start_row = df.loc[row_num - 1]
+        end_row = df.loc[row_num]
+        start_item = start_row[col_name]
+        end_item = end_row[col_name]
+        return end_item - start_item
+    
+    def nurse_dst_walked(self, nurse_id):
+        move_rows = self.nurse_df[(self.nurse_df['nurse'] == nurse_id) & (self.nurse_df['action'] == "move to")].index.tolist()
+        total_dst = 0
+        for row in move_rows:
+            x_dst = self.row_diff(row, self.nurse_df, "x")
+            y_dst = self.row_diff(row, self.nurse_df, "y")
+            distance = sqrt(x_dst ** 2 + y_dst ** 2)
+            total_dst += distance
+        
+        return total_dst
+    
+    def nurse_time_walked(self, nurse_id):
+        move_rows = self.nurse_df[(self.nurse_df['nurse'] == nurse_id) & (self.nurse_df['action'] == "move to")].index.tolist()
+        total_time = 0
+        for row in move_rows:
+            total_time += self.row_diff(row, self.nurse_df, "time")
+        return total_time
+    
+    def nurse_time_at_patient(self, nurse_id, patient_id):
+        #assuming the df is sorted by nurse_id first and time second
+        time_rows = self.nurse_df[(self.nurse_df['nurse'] == nurse_id) & (self.nurse_df['patient'] == patient_id) & (self.nurse_df['action'] == "time at patient")].index.tolist()
+        total_time = 0
+        for row in time_rows:
+            total_time += self.row_diff(row, self.nurse_df, "time")
+        return total_time
+    
+    def nurse_time_at_patients(self, nurse_id, patient_ids):
+        total_time = 0
+        for patient_id in patient_ids:
+            total_time += self.nurse_time_at_patient(nurse_id, patient_id)
+        return total_time
+    
+    def nurse_time_at_own_patients(self, nurse_id):
+        this_nurses_patients = self.nurse_patients[nurse_id]
+        return self.nurse_time_at_patients(nurse_id, this_nurses_patients)
+    
+    def nurse_time_at_other_patients(self, nurse_id):
+        other_patients = []
+        for i, patient_lst in enumerate(self.nurse_patients):
+            if i != nurse_id:
+                other_patients += patient_lst
+        
+        return self.nurse_time_at_patients(nurse_id, other_patients)
+    
+    def nurse_time_at_all_patients(self, nurse_id):
+        flattened_patients_lst =  list(chain.from_iterable(self.nurse_patients))
+        return self.nurse_time_at_patients(nurse_id, flattened_patients_lst)
+    
+    def nurse_time_resting(self, nurse_id):
+        end_time = self.nurse_df["time"].max() #assuming it started at time 0
+        resting_time = end_time - self.nurse_time_at_all_patients(nurse_id) - self.nurse_time_walked(nurse_id)
+        return resting_time
+    
+    def patient_time_waiting(self, patient_id):
+        ...
+
+
+        
