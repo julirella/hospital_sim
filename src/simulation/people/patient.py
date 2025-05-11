85161f05a097a2a39f9d1524ea6b07367c484ae5
Julia Grandcourtova <grandjul@fit.cvut.cz>
2025-05-11 11:46:10 +0200

    cleanup and add comments to people

--- src/simulation/people/patient.py
+++ src/simulation/people/patient.py
@@ -4,7 +4,15 @@ from src.simulation.sim_time import SimTime
 
 
 class Patient:
+    """
+    class representing a patient
+    """
     def __init__(self, patient_id: int, nurse: Nurse, room: PatientRoom) -> None:
+        """
+        :param patient_id: ID of the patient
+        :param nurse: the patient's assigned nurse
+        :param room: the room in which the patient is located
+        """
         self._patient_id = patient_id
         self._nurse = nurse
         self._room = room
