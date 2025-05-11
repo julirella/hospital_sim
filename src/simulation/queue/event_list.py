7e1de00304dfc1568d9d0846bacc602531a1f2ce
Julia Grandcourtova <grandjul@fit.cvut.cz>
2025-05-11 15:06:46 +0200

    cleanup and add comments to queues

--- src/simulation/queue/event_list.py
+++ src/simulation/queue/event_list.py
@@ -5,13 +5,22 @@ from typing import TypeVar, Generic
 E = TypeVar("E", bound=Event)
 
 class ListEvent:
+    """
+    wrapper around event for storage in linked list
+    """
     def __init__(self, event: E, next_event):
         self.event = event
         self.next: ListEvent | None = next_event
 
-#linked list of events
 class EventList(Generic[E]):
+    """
+    linked list of events sorted by time of occurrence
+    """
     def __init__(self, events: list[E]):
+        """
+        :param events: regular list of events to be stored
+        """
+
         #build linked list
         events.sort(key=lambda x: x.time)
         prev = None
@@ -20,21 +29,36 @@ class EventList(Generic[E]):
         self._front: ListEvent | None = prev
 
     def empty(self) -> bool:
+        """
+        :return: True if the list is empty, else False
+        """
         return self._front is None
 
     def front(self) -> E | None:
+        """
+        access the front of the list
+        :return: the front event or None if the list is empty
+        """
         if self._front is None:
             return None
         else:
             return self._front.event
 
     def pop_front(self) -> E | None:
+        """
+        remove and return the front event of the list
+        :return: the front event or None if the list is empty
+        """
         event = self.front()
         if event is not None:
             self._front = self._front.next
         return event
 
     def next_time(self) -> float | None:
+        """
+        access the time of the front event
+        :return: the time of the front event or None if the list is empty
+        """
         if self.empty():
             return None
         return self.front().next_time()
--- src/simulation/queue/nurse_list.py
+++ src/simulation/queue/nurse_list.py
@@ -2,17 +2,26 @@ from src import SimTime, Nurse, Graph
 from src.simulation.timed_object import EventStatus, TimedNurseId, ReturnToOffice, Event
 from src.simulation.queue.event_list import EventList, ListEvent
 
-#linkded list of nurse events
 class NurseList(EventList[Event]):
+    """
+    class representing a linked list of events in a nurse queue
+    """
     def __init__(self, events: list[Event], sim_time: SimTime, nurse: Nurse, max_graph_dst: float, graph: Graph):
+        """
+        :param events: events to be stored in queue
+        :param sim_time: SimTime object to track simulation time
+        :param nurse: nurse to whom the queue belongs
+        :param max_graph_dst: max distance that can be walked in graph
+        :param graph: department graph
+        """
         super().__init__(events)
         self._sim_time: SimTime = sim_time
         self._graph = graph
         self._nurse: Nurse = nurse
         self._nurse_id = nurse.nurse_id
         self._max_walk_time = max_graph_dst / nurse.speed #longest walk time for nurse between any two nodes in graph
-        self._timed_nurse_id: TimedNurseId
-        self._event_logs = []
+        self._timed_nurse_id: TimedNurseId # combination of time of first event and the nurse's ID
+        self._event_logs = [] # list for logs of finished events
         self._in_global_queue: bool = False
 
     @property
@@ -32,6 +41,7 @@ class NurseList(EventList[Event]):
         self._in_global_queue = value
 
     def __max_event_duration__(self, event: Event) -> float:
+        # maximum duration of an event including time to walk there
         return event.duration + self._max_walk_time
 
     def __insert_after__(self, new_event: Event, pred_event: ListEvent=None) -> None:
@@ -59,19 +69,26 @@ class NurseList(EventList[Event]):
         current_event = self._front.event
         if current_event.type != 'return_to_office':
             raise Exception("can't replace event that isn't return to office")
-        # if current_event.status == EventStatus.ACTIVE:
+
         current_event.pause()
         finished_log = self.pop_front().log
         self._event_logs += finished_log
         self.__insert_after__(event)
 
     def has_time_now(self, event: Event) -> bool:
+        """
+        checks if nurse currently has enough free time to deal with event including walking there
+        :param event: the event to be dealt with
+        :return: True if nurse has enough time, else False
+        """
+
         if self.empty():
             return True
 
         front_event_time = self._front.event.time
 
         # if the first event is return to office, the event after that is considered as the first event
+        # because return to office can always be cancelled
         if self.front().type == 'return_to_office':
             if self._front.next is None:
                 return True
@@ -81,17 +98,23 @@ class NurseList(EventList[Event]):
         return self.empty() or self.__max_event_duration__(event) <= front_event_time - self._sim_time.sim_time
 
     def current_event_level(self) -> int:
+        """
+        :return: if current event is a request, then its level, else -1 (even if the queue is empty)
+        """
         if self.empty():
             return -1
 
         current_event = self._front.event
         if current_event.status == EventStatus.ACTIVE and current_event.type == 'request':
-            return current_event.level #TODO: maybe just get level from event too?
+            return current_event.level
         else:
             return -1
 
-    #find gap in queue to fit event and add it there
     def add_to_gap(self, event: Event) -> None:
+        """
+        finds a gap in the list big enough for event (including walk time) and inserts it there
+        :param event: the event to be added
+        """
         if self.empty():
             self.__insert_after__(event)
             return
@@ -123,14 +146,19 @@ class NurseList(EventList[Event]):
         if not done:
             self.__insert_after__(event, prev_event)
 
-    #add event after end of current running event
     def add_after_current(self, event: Event) -> None:
+        """
+        If there is currently a running event, adds the new event behind it, pushing back subsequent events. If there
+        is no running event or the first event is return to office, adds it to start.
+        :param event: the event to be added
+        """
         if self.empty():
             self.__insert_after__(event)
             return
 
         current_event: Event = self.front()
         if current_event.type == 'return_to_office':
+            # running return to office just gets cancelled
             self.__replace_return_to_office__(event)
         elif current_event.status == EventStatus.NOT_STARTED:
             # it just goes straight away
@@ -139,6 +167,11 @@ class NurseList(EventList[Event]):
             self.__insert_after__(event, self._front)
 
     def add_to_start(self, event: Event) -> None:
+        """
+        Adds new event to the start of the list, pausing the first event if it is running and pushing back all
+        events in queue.
+        :param event: the event to be added
+        """
         if self.empty():
             self.__insert_after__(event)
             return
@@ -146,7 +179,8 @@ class NurseList(EventList[Event]):
         current_event: Event = self.front()
 
         if current_event.type == 'return_to_office':
-            current_event.pause() # return to office can be stopped even if it hasn't started yet
+            # return to office can just be cancelled (even if it hasn't started yet)
+            current_event.pause()
             finished_log = self.pop_front().log
             self._event_logs += finished_log
 
@@ -159,14 +193,18 @@ class NurseList(EventList[Event]):
 
 
     def run_next_step(self) -> bool:
-        #returns true if step run was last step of event (aka event was finished)
+        """
+        run the next step of the first event in list, removing first event if it is finished in the process
+        :return: True if this finishes the current event, else False
+        """
+
         if self.empty():
             raise Exception("can't run next step of emtpy list")
 
         #call run next step of top event
         next_event: Event = self.front()
         finished: bool = next_event.run_next_step()
-        #if the event is over, remove it (and maybe log that)
+        #if the event is over, remove it
         if finished: # don't return again if already returned to office
             finished_log = self.pop_front().log
             self._event_logs += finished_log
@@ -178,8 +216,15 @@ class NurseList(EventList[Event]):
         return finished
 
     def create_timed_nurse_id(self) -> TimedNurseId:
+        """
+        create a TimedNurseId object for the combination of the time of the next event and the nurse's ID
+        :return: the created TimedNurseId
+        """
         self._timed_nurse_id = TimedNurseId(self.next_time(), self._nurse_id)
         return self._timed_nurse_id
 
     def current_timed_nurse_id(self) -> TimedNurseId:
+        """
+        :return: current TimedNurseId representing the time of the next event in the list and the nurse's ID
+        """
         return self._timed_nurse_id
